91efdb7b17d2eb81aade0d86da2ddb38edc2bf92
yanuvelir <yanuvelir@gmail.com>
2024-05-24 16:36:27 +0300

    added new branch

--- src/GroupOfAuto.java
+++ src/GroupOfAuto.java
@@ -1,19 +1,35 @@
 public class GroupOfAuto {
 
     private Auto[] group;
-    public GroupOfAuto(int amount){
+
+    public GroupOfAuto(int amount) {
         group = new Auto[amount];
     }
 
-    public void Add(Auto automobile){
+    public void add(Auto automobile) {
+        // Check if the group is already full
+        boolean groupFull = true;
+        for (Auto auto : group) {
+            if (auto == null) {
+                groupFull = false;
+                break;
+            }
+        }
 
-        for(int i=0; i<group.length; i++){
-            if(group[i] == null){
+        if (groupFull) {
+            System.out.println("Exceeded amount of group");
+            return;
+        }
+
+        // Add the automobile to the group
+        for (int i = 0; i < group.length; i++) {
+            if (group[i] == null) {
                 group[i] = automobile;
-                System.out.println("Auto " +automobile.getClass().getName()+ "" + automobile.getName()
-                        + " has added to the group. The order number is: " + (i+1));
+                System.out.println("Auto " + automobile.getClass().getName() + " " + automobile.getName() +
+                        " has been added to the group. The order number is: " + (i + 1));
                 break;
             }
         }
     }
 }
+
